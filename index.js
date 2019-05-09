5b407abac6fc7ebe830e6272deeefb228020f45d
Emma Englishby <emma@englishby.co>
2019-05-09 15:56:02 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+window.onload = function(){
+
+  document.querySelector(".fade-in").classList.add("load");
+
+
+};
+
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
